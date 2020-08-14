debcbeb8d07546ed6bfafa0de231e23d922c71f6
Shawn Seymour <shawn@devshawn.com>
2020-08-14 17:14:20 -0500

    release: 0.2.7!

--- kafka-gitops.rb
+++ kafka-gitops.rb
@@ -1,9 +1,9 @@
 class KafkaGitops < Formula
     desc "Manage Apache Kafka topics and generate ACLs through a desired state file"
     homepage "https://github.com/devshawn/kafka-gitops"
-    url "https://github.com/devshawn/kafka-gitops/releases/download/0.2.6/kafka-gitops.zip"
-    sha256 "cea3353ddfc76d6777b5dae0de845dcc61e1279c6f73096672645c81f0d8c790"
-    version "0.2.6"
+    url "https://github.com/devshawn/kafka-gitops/releases/download/0.2.7/kafka-gitops.zip"
+    sha256 "2030cf6872bce54884195a7861504d5b8d2c44bef604af98f418e0c95850e31e"
+    version "0.2.7"
 
     bottle :unneeded
   
