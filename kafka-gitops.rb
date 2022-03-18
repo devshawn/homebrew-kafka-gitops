c86089abf0c4e76d2a25c4390dc824728b60d587
Shawn Seymour <shawn@devshawn.com>
2022-03-18 17:05:54 -0500

    chore: remove unneeded line

--- kafka-gitops.rb
+++ kafka-gitops.rb
@@ -4,8 +4,6 @@ class KafkaGitops < Formula
     url "https://github.com/devshawn/kafka-gitops/releases/download/0.2.14/kafka-gitops.zip"
     sha256 "6794d29feca2512a6436393d62672da010839440e6a34be5ac1697e95e422c1a"
     version "0.2.14"
-
-    bottle :unneeded
   
     def install
       bin.install "kafka-gitops"
